515d5a75f94cd71e8002d247e3a9fab0f9bd472f
kuen229 <kuenzhang@gmail.com>
2018-12-29 22:31:34 +0800

    Add Windows Demo

---
+++ Windows/Config.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <stdint.h>
+
+struct UserInfo
+{
+    std::string userId;
+    std::string userSig;
+};
+
+class Config
+{
+protected:
+    Config();
+    Config(const Config&);
+    Config operator =(const Config&);
+public:
+    ~Config();
+
+    static Config& instance();
+
+    bool load();
+
+    uint32_t getSdkAppId() const;
+    std::vector<UserInfo> getUserInfos() const;
+private:
+    uint32_t m_sdkAppId;
+    std::vector<UserInfo> m_userInfos;
+};
