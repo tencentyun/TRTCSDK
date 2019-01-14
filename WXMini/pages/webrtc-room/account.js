32b2e933b7903dc64f3a5bc00e1e4ccf570fe628
anderlu <anderlublaze@gmail.com>
2019-01-14 20:11:24 +0800

    修改小程序demo

--- WXMini/pages/webrtc-room/account.js
+++ WXMini/pages/webrtc-room/account.js
@@ -1,45 +1,27 @@
 //将 TEST_ACCOUNT 的内容替换成"控制台/实时音视频/快速上手第三步"生成的配置文件内容
-// const TEST_ACCOUNT = {
-//   "sdkappid": 1400037025,
-//   "users": [{
-//       "userId": "mini_test_01",
-//       "userToken": "eJxlj11PgzAYhe-5FYRrY9pCMXo3mDMDnDDnGr1pkBb2QmCVVt1H9t9VXCKJ5-Z5ck7O0bJt21klj5d5UWzfO8PNXknHvrEd5Fz8QaVA8Nxwtxf-oNwp6CXPSyP7AWJKKUFo7ICQnYESzkYLHXAjteEIjywtGj5M-dZ43x3uFSJ0rEA1wPvb53CehfqtgLTx-UPMvLq8jgq6fq1j5sqqbHLNRJYnbffyELiomm8m0SJj6ScjswTSaDZdqLTe7JYTJsowiPXTAS-NNAjV6m6vR5MGWnn*5VGCMfLJiH7IXsO2GwSCMMXERT9xrJP1BXoAYCo_"
-//     },
-//     {
-//       "userId": "mini_test_02",
-//       "userToken": "eJxlj81Og0AYRfc8BWFtZH46rW3iAhCVFpoYW42rybR8wFfDT4ZppTW*u0qbSOLdnpN7cz8t27adVfx8rbbbel8ZaY4NOPbMdohz9QebBlOpjOQ6-Qeha1CDVJkB3UMqhGCEDB1MoTKY4cUosUJpoDWSsIHVpu*ynzrXjH46*IQwMVQw72ESroPIf9TqNQauF5NiE8z3cZMtCq3Kzt21nesXyTpfqfH0DVzqYeh93POXeRUUp1DoEg7LPE28eFwrP-busJ0mYvcUZQ-HzTKIbgeTBku4-BoJRim54QN6AN1iXfUCI1RQxslvHOvL*gZZ0V*3"
-//     },
-//     {
-//       "userId": "mini_test_03",
-//       "userToken": "eJxlj0tvgkAUhff8CsK6ae7MMKhNXPDQvjQEkdR0M6EyNLcITodpozT9723RRJLe7fflnHO-LNu2nfUivc632-1HY4Q5KunYN7YDztUFKoWFyI1guvgH5UGhliIvjdQ9JJxzCjB0sJCNwRLPRo0NCiNbI4ANrLaoRF91inF-M9gIKB8q*NrD5SwJ7-2omx0yUGFa4xMEa7*629A5SK8KVm95tqOh171E*nbMEh*DLItl7S4V0eZhEtE4Sd8XpRqNcfXIn*eFF*dH6m92nSLJdDqoNFjL818up4TAZDjoU*oW900vUCCcUAZ-51jf1g*obV5Z"
-//     },
-//     {
-//       "userId": "mini_test_04",
-//       "userToken": "eJxlj8FOg0AURfd8BWFbY98A0yFNXEAlFsssTDW1biZQBnihwGQYTRvjv6vYRJK*7Tm5975Py7Zt5znd3maHQ--eGWHOSjr20nbAufmHSmEhMiM8XVxBeVKopchKI-UICaXUBZg6WMjOYIkXo8UOhZGDEeBPrKFoxFj1F*P-ZHgMXDpVsBohj19Wyfo1YLtg8Xji0XCf8AQhD4563TdxXVWzzVPBvbDcU8VYHWIcwlwd0yp6aHMW5v3cxDumOV8o8rZJcbY668jU*1xu4wbuJpUGW3n5y6cuIQSmgz6kHrDvRsEFQonrwe851pf1DZ4VXmI_"
-//     },
-//     {
-//       "userId": "mini_test_05",
-//       "userToken": "eJxlj0tPg0AYRff8CsJWI8NjQjRxQbQYG2poa1VWE9oZysdrxmEG*4j-3YpNxHi35*Te3KNhmqb1HC*vss2G61YRtRfMMm9MC1mXv1AIoCRTxJP0H2Q7AZKRLFdMDtDBGLsIjR2grFWQw9looAWiWKcIwiOroxUZpn5q-FOHFyD3jwLbAc4mq7vHqKq2ST8VXYmjna5SFKSrohBNnXDbr-PDOrlfvGv*UZWLECZhkV3Hdqhpvbb35XwmX5qp9tL5Q5y*1k8XuV4iO*p9*naI*e1oUkHDzr987DqnBCPaM9kBbwfBRQ52XA99xzI*jS8RYGEq"
-//     }
-//   ]
-// }
-
 const TEST_ACCOUNT = {
-  "sdkappid": 1400169294,
+  "sdkappid": 1400037025,
   "users": [{
-    "userId": "Miniprogram_trtc_01",
-    "userToken": "eJxlj1FPgzAUhd-5FYRn41qgDfg2QRMmE3CLc7w0hRa84qDpqm4a-7uRLZHE*-p9OefcL8u2bWedri55XQ9vvWHmqKRjX9kOci7*oFIgGDfM0*IflAcFWjLeGKlHiAkhLkJTB4TsDTRwNpbQg9JDq-mOGW1qhvBE3ouOjY2nNB8hTEM39KcKtKegm22UFLFYZuUshVcTRGE*rGL4rF5Ui54EzqqkzlU-6ySGNDneF8nzPL*mQa0XNN5UYbDZyuixlLdqwYuBCzq-K5t1hx4ygj-0YVJpYCfP7-k08BBxp5vfpd7D0I*CizDBrod*z7G*rR*sS2Ja"
-  }, {
-    "userId": "Miniprogram_trtc_02",
-    "userToken": "eJxlj11PgzAYhe-5FYRbjL4tZTITL9gciBPRuGbbVVOhJc0cdKVzfsT-bmRLJPHcPk-OyflyXNf1FvfP57ws231jmf3QwnOvXA*8sz*otaoYtyww1T8o3rUygnFphekhCsMQAwwdVYnGKqlORq4apU1bG75l1tiSAR7IXbVh-eKxjQCg0RiPyVBR9bFoRqfZ0w0RD0j6y7lJXy53MJnmRSeXqItiExN7WNfVBU1JnHAi66x*BHN3u5qghGfVxviUFjaiyecMx36*X8tXtNLFfEF2NIXrwaRVW3G6R0ZRACFGA-omTKfaphcwoBDhAH7jOd-OD-ywYN4_"
-  }, {
-    "userId": "Miniprogram_trtc_03",
-    "userToken": "eJxlj11PgzAARd-5FYRXjOkHRTDxwTA0xG1I2GLmS9PRDquu1LYbbsb-bmRLJPG*npN7c7883-eDxbS*ZE3T7ZSj7qBF4F-7AQgu-qDWklPmKDb8HxSfWhpB2cYJM0BICEEAjB3JhXJyI8-GTCqpTdcatqXOuIYCPJItf6PD4qktAgDGKUqjsSLbU1G*zIoqe3jXzR6GjyxSbL1aH2YEv5ZPL-bIxcTWnPFk2nyE5aS3fdHepyWM3BzeVaTOd6Gq5uT2*MwKjVWc5MYuSrzMrtBKhf3NaNLJrTjfi*IEA4LgiO6FsbJTg4AAJBBh8JvA*-Z*AK5rYg8_"
-  }, {
-    "userId": "Miniprogram_trtc_04",
-    "userToken": "eJxlj81Og0AYRfc8BWFt7AwDWEy6AGpqbYlia6urCT8DfFaGYTrQNsZ3N9ImYrzbc3Jv7qem67qxXq6u4zStW66oOglm6Le6gYyrXygEZDRWlMjsH2RHAZLROFdM9hDbtm0iNHQgY1xBDhcjBA5C1oWMK6qkSimyBvI*29F*8dxmIYQd13T-KFCci*6iYO6NeVWGYUGaRCpe7Wa4i6ZNjpJ5uQ1l5N*34uF4cBbPy4UHfum*B9PDB8nJo29uOrUdvW1O8vXlaZTAOvFmKydrblrWBGkxmQwmFVTscs9yxgTZJh7Qjsk91LwXTIRtbBL0E0P70r4BHx1jNA__"
-  }],
-  "roles": []
+      "userId": "mini_test_01",
+      "userToken": "eJxlj11PgzAYhe-5FYRrY9pCMXo3mDMDnDDnGr1pkBb2QmCVVt1H9t9VXCKJ5-Z5ck7O0bJt21klj5d5UWzfO8PNXknHvrEd5Fz8QaVA8Nxwtxf-oNwp6CXPSyP7AWJKKUFo7ICQnYESzkYLHXAjteEIjywtGj5M-dZ43x3uFSJ0rEA1wPvb53CehfqtgLTx-UPMvLq8jgq6fq1j5sqqbHLNRJYnbffyELiomm8m0SJj6ScjswTSaDZdqLTe7JYTJsowiPXTAS-NNAjV6m6vR5MGWnn*5VGCMfLJiH7IXsO2GwSCMMXERT9xrJP1BXoAYCo_"
+    },
+    {
+      "userId": "mini_test_02",
+      "userToken": "eJxlj81Og0AYRfc8BWFtZH46rW3iAhCVFpoYW42rybR8wFfDT4ZppTW*u0qbSOLdnpN7cz8t27adVfx8rbbbel8ZaY4NOPbMdohz9QebBlOpjOQ6-Qeha1CDVJkB3UMqhGCEDB1MoTKY4cUosUJpoDWSsIHVpu*ynzrXjH46*IQwMVQw72ESroPIf9TqNQauF5NiE8z3cZMtCq3Kzt21nesXyTpfqfH0DVzqYeh93POXeRUUp1DoEg7LPE28eFwrP-busJ0mYvcUZQ-HzTKIbgeTBku4-BoJRim54QN6AN1iXfUCI1RQxslvHOvL*gZZ0V*3"
+    },
+    {
+      "userId": "mini_test_03",
+      "userToken": "eJxlj0tvgkAUhff8CsK6ae7MMKhNXPDQvjQEkdR0M6EyNLcITodpozT9723RRJLe7fflnHO-LNu2nfUivc632-1HY4Q5KunYN7YDztUFKoWFyI1guvgH5UGhliIvjdQ9JJxzCjB0sJCNwRLPRo0NCiNbI4ANrLaoRF91inF-M9gIKB8q*NrD5SwJ7-2omx0yUGFa4xMEa7*629A5SK8KVm95tqOh171E*nbMEh*DLItl7S4V0eZhEtE4Sd8XpRqNcfXIn*eFF*dH6m92nSLJdDqoNFjL818up4TAZDjoU*oW900vUCCcUAZ-51jf1g*obV5Z"
+    },
+    {
+      "userId": "mini_test_04",
+      "userToken": "eJxlj8FOg0AURfd8BWFbY98A0yFNXEAlFsssTDW1biZQBnihwGQYTRvjv6vYRJK*7Tm5975Py7Zt5znd3maHQ--eGWHOSjr20nbAufmHSmEhMiM8XVxBeVKopchKI-UICaXUBZg6WMjOYIkXo8UOhZGDEeBPrKFoxFj1F*P-ZHgMXDpVsBohj19Wyfo1YLtg8Xji0XCf8AQhD4563TdxXVWzzVPBvbDcU8VYHWIcwlwd0yp6aHMW5v3cxDumOV8o8rZJcbY668jU*1xu4wbuJpUGW3n5y6cuIQSmgz6kHrDvRsEFQonrwe851pf1DZ4VXmI_"
+    },
+    {
+      "userId": "mini_test_05",
+      "userToken": "eJxlj0tPg0AYRff8CsJWI8NjQjRxQbQYG2poa1VWE9oZysdrxmEG*4j-3YpNxHi35*Te3KNhmqb1HC*vss2G61YRtRfMMm9MC1mXv1AIoCRTxJP0H2Q7AZKRLFdMDtDBGLsIjR2grFWQw9looAWiWKcIwiOroxUZpn5q-FOHFyD3jwLbAc4mq7vHqKq2ST8VXYmjna5SFKSrohBNnXDbr-PDOrlfvGv*UZWLECZhkV3Hdqhpvbb35XwmX5qp9tL5Q5y*1k8XuV4iO*p9*naI*e1oUkHDzr987DqnBCPaM9kBbwfBRQ52XA99xzI*jS8RYGEq"
+    }
+  ]
 }
 
 module.exports = TEST_ACCOUNT;
