b7250b5d8b279c9c170a5871e31b07b3f1875e2f
anderlu <anderlublaze@gmail.com>
2020-02-28 17:07:21 +0800

    fix: 修复stream reset 逻辑

--- WXMini/components/trtc-room/model/stream.js
+++ WXMini/components/trtc-room/model/stream.js
@@ -18,9 +18,9 @@ class Stream {
     Object.assign(this, options)
   }
   reset() {
-    if (!this.playerContext) {
+    if (this.playerContext) {
       this.playerContext.stop()
-      this.playerContext = null
+      this.playerContext = undefined
     }
     Object.assign(this, DEFAULT_PLAYER_CONFIG, {
       userID: '', // 该stream 关联的userID
