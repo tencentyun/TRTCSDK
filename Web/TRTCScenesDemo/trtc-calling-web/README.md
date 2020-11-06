ffb1d5acbf69043838735ac9e54288c05a5d8ae8
easinyao <easinyao@tencent.com>
2020-11-06 10:56:13 +0800

    feat: add trtccalling demo

---
+++ Web/TRTCScenesDemo/trtc-calling-web/README.md
@@ -0,0 +1,204 @@
+本文主要介绍如何快速跑通 Web 版本的 TRTCCalling Demo，Demo 中包括语音通话和视频通话场景：
+
+\- 语音通话：纯语音交互，支持多人互动语音聊天。
+
+\- 视频通话：视频通话，面向在线客服等需要面对面交流的沟通场景。
+
+### 环境要求
+* 请使用最新版本的 Chrome 浏览器。
+* TRTCCalling 依赖以下端口进行数据传输，请将其加入防火墙白名单，配置完成后，您可以通过访问并体验 [官网 Demo](https://demo-1252463788.cos.ap-shanghai.myqcloud.com/trtccalling/demo/index.html) 检查配置是否生效。
+  - TCP 端口：8687
+  - UDP 端口：8000，8080，8800，843，443，16285
+  - 域名：qcloud.rtc.qq.com
+
+> 
+>- 一般情况下体验 Demo 需要部署至服务器，通过 https://域名/xxx 访问，或者直接在本地搭建服务器，通过 localhost:端口访问。
+>- 目前桌面端 Chrome 浏览器支持 TRTC 桌面浏览器 SDK 的相关特性比较完整，因此建议使用 Chrome 浏览器进行体验。
+
+### 前提条件
+
+您已 [注册腾讯云](https://cloud.tencent.com/document/product/378/17985) 账号，并完成 [实名认证](https://cloud.tencent.com/document/product/378/3629)。
+
+### 复用 Demo 的 UI 界面
+
+<span id="step1"></span>
+
+#### 步骤1：创建新的应用
+
+1. 登录实时音视频控制台，选择【开发辅助】>【[快速跑通Demo](https://console.cloud.tencent.com/trtc/quickstart)】。
+
+2. 单击【立即开始】，输入应用名称，例如`TestTRTC`，单击【创建应用】。
+
+<span id="step2"></span>
+
+#### 步骤2：下载 SDK 和 Demo 源码
+2. 鼠标移动至对应卡片，单击【[Github](https://github.com/tencentyun/TRTCSDK/tree/master/Web/TRTCScenesDemo/trtc-calling-web)】跳转至 Github（或单击【[ZIP](https://liteavsdk-1252463788.cos.ap-guangzhou.myqcloud.com/H5_latest.zip)】），下载相关 SDK 及配套的 Demo 源码。
+ ![](https://main.qcloudimg.com/raw/0f35fe3bafe9fcdbd7cc73f991984d1a.png)
+2. 下载完成后，返回实时音视频控制台，单击【我已下载，下一步】，可以查看 SDKAppID 和密钥信息。
+
+<span id="step3"></span>
+
+#### 步骤3：配置 Demo 工程文件
+
+1. 解压 [步骤2](#step2) 中下载的源码包。
+
+2. 找到并打开`Web/TRTCScenesDemo/TRTCCalling/public/debug/GenerateTestUserSig.js`文件。
+
+3. 设置`GenerateTestUserSig.js`文件中的相关参数：
+
+  <ul><li>SDKAPPID：默认为0，请设置为实际的 SDKAppID。</li>
+
+  <li>SECRETKEY：默认为空字符串，请设置为实际的密钥信息。</li></ul> 
+
+  <img src="https://main.qcloudimg.com/raw/0ae7a197ad22784384f1b6e111eabb22.png">
+
+4. 返回实时音视频控制台，单击【粘贴完成，下一步】。
+
+5. 单击【关闭指引，进入控制台管理应用】。
+
+>本文提到的生成 UserSig 的方案是在客户端代码中配置 SECRETKEY，该方法中 SECRETKEY 很容易被反编译逆向破解，一旦您的密钥泄露，攻击者就可以盗用您的腾讯云流量，因此****该方法仅适合本地跑通 Demo 和功能调试****。
+
+>正确的 UserSig 签发方式是将 UserSig 的计算代码集成到您的服务端，并提供面向 App 的接口，在需要 UserSig 时由您的 App 向业务服务器发起请求获取动态 UserSig。更多详情请参见 [服务端生成 UserSig](https://cloud.tencent.com/document/product/647/17275#Server)。
+
+#### 步骤4：运行 Demo
+>- 同步依赖： npm install
+>- 启动项目： npm run serve
+>- 浏览器中打开链接：http://localhost:8080/
+
+- Demo 运行界面如图所示：
+![](https://main.qcloudimg.com/raw/90118deded971621db7bb14b55073bcc.png)
+- 输入用户 userid，点击【登录】
+![](https://main.qcloudimg.com/raw/f430fb067cddbb52ba32e4d0660cd331.png)
+- 输入呼叫用户 userid，即可视频通话
+![](https://main.qcloudimg.com/raw/66562b4c14690de4eb6f2da58ee6f4df.png)
+- 视频通话
+![](https://main.qcloudimg.com/raw/592189d0f18c91c51cdf7184853c6437.png)
+
+
+### 实现自定义 UI 界面
+#### 步骤1：集成 SDK
+NPM 集成
+```javascript
+//TRTCCalling SDK
+npm install trtc-calling-js --save
+```
+在项目脚本里引入模块
+```javascript
+import TrtcCalling from 'trtc-calling-js';
+```
+#### 步骤2：创建 trtcCalling 对象
+>- sdkAppID: 您从腾讯云申请的 sdkAppID
+```javascript
+let options = {
+  SDKAppID: 0 // 接入时需要将0替换为您的云通信应用的 SDKAppID
+};
+const trtcCalling = new TRTCCalling(options);
+```
+
+#### 步骤3：登录
+>- userID: 用户 ID
+>- userSig: 用户签名，计算方式参见[如何计算 userSig](https://cloud.tencent.com/document/product/647/17275)
+```javascript
+trtcCalling.login({
+  userID,
+  userSig
+});
+```
+
+#### 步骤4：实现 1v1 通话
+>#### 拨打：
+>- userID: 用户 ID
+>- type: 通话类型，0-未知， 1-语音通话，2-视频通话
+>- timeout: 邀请超时, 单位 s(秒)
+```javascript
+trtcCalling.call({
+  userID,
+  type: 2,
+  timeout
+});
+```
+>#### 接听
+>- inviteID: 邀请 ID, 标识一次邀请
+>- roomID: 通话房间号 ID
+>- callType: 0-未知， 1-语音通话，2-视频通话
+```javascript
+trtcCalling.accept({
+  inviteID,
+  roomID,
+  callType
+});
+```
+>#### 打开本地摄像头
+```javascript
+trtcCalling.openCamera()
+```
+>#### 展示远端画面
+>- userID: 远端用户 ID
+>- videoViewDomID: 该用户数据将渲染到该 DOM ID 节点里
+```javascript
+trtcCalling.startRemoteView({
+  userID,
+  videoViewDomID
+})
+```
+
+>#### 展示本地画面
+>- userID: 本地用户 ID
+>- videoViewDomID: 该用户数据将渲染到该 DOM ID 节点里
+```javascript
+trtcCalling.startLocalView({
+  userID,
+  videoViewDomID
+})
+```
+
+>#### 挂断/拒接
+```javascript
+trtcCalling.hangup()
+```
+>- inviteID: 邀请 id，标识一次邀请
+>- isBusy: 是否是忙线中， 0-未知， 1-语音通话，2-视频通话
+```javascript
+trtcCalling.reject({ 
+  inviteID,
+  isBusy
+  })
+```
+
+### 支持的平台
+
+| 操作系统 |      浏览器类型      | 浏览器最低版本要求 |
+| :------: | :------------------: | :----------------: |
+|  Mac OS  | 桌面版 Safari 浏览器 |        11+         |
+|  Mac OS  | 桌面版 Chrome 浏览器 |        56+         |
+| Windows  | 桌面版 Chrome 浏览器 |        56+         |
+| Windows  |   桌面版 QQ 浏览器   |        10.4        |
+
+### 常见问题
+
+#### 1. 查看密钥时只能获取公钥和私钥信息，该如何获取密钥？
+TRTC SDK 6.6 版本（2019年08月）开始启用新的签名算法 HMAC-SHA256。在此之前已创建的应用，需要先升级签名算法才能获取新的加密密钥。如不升级，您也可以继续使用 老版本算法 ECDSA-SHA256，如已升级，您按需切换为新旧算法。
+
+升级/切换操作：
+
+1. 登录 实时音视频控制台。
+
+2. 在左侧导航栏选择【应用管理】，单击目标应用所在行的【应用信息】。
+
+3. 选择【快速上手】页签，单击【第二步 获取签发 UserSig 的密钥】区域的【点此升级】、【非对称式加密】或【HMAC-SHA256】。
+
+- 升级：
+
+   ![](https://main.qcloudimg.com/raw/69bd0957c99e6a6764368d7f13c6a257.png)
+
+- 切换回老版本算法 ECDSA-SHA256：
+
+   ![](https://main.qcloudimg.com/raw/f89c00f4a98f3493ecc1fe89bea02230.png)
+
+- 切换为新版本算法 HMAC-SHA256：
+
+   ![](https://main.qcloudimg.com/raw/b0412153935704abc9e286868ad8a916.png)
+
+#### 2. 防火墙有什么限制？
+
+由于 SDK 使用 UDP 协议进行音视频传输，所以对 UDP 有拦截的办公网络下无法使用，如遇到类似问题，请参考文档：[应对公司防火墙限制](https://cloud.tencent.com/document/product/647/34399)。
---
+++ Web/TRTCScenesDemo/trtc-calling-web/src/components/video-call/index.vue
@@ -0,0 +1,300 @@
+<template>
+  <div class="video-call-section">
+    <div
+      class="video-call-section-header"
+    >Welcome {{loginUserInfo && (loginUserInfo.name || loginUserInfo.userId)}}</div>
+    <div class="video-call-section-title">视频通话</div>
+    <search-user @callUser="handleCallUser" @cancelCallUser="handleCancelCallUser"></search-user>
+    <div :class="{ 'video-conference': true, 'is-show': isShowVideoCall }">
+      <div class="video-conference-header">视频通话区域</div>
+
+      <div class="video-conference-list">
+        <div
+          v-for="userId in meetingUserIdList"
+          :key="`video-${userId}`"
+          :id="`video-${userId}`"
+          :class="{'user-video-container': true, 'is-me': userId === loginUserInfo.userId}"
+        >
+          <div class="user-status">
+            <div
+              :class="{'user-video-status': true, 'is-mute': isUserMute(muteVideoUserIdList, userId)}"
+            ></div>
+            <div
+              :class="{'user-audio-status': true, 'is-mute': isUserMute(muteAudioUserIdList, userId)}"
+            ></div>
+          </div>
+          <div class="video-item-username">{{userId2Name[userId] || userId}}</div>
+        </div>
+      </div>
+      <div class="video-conference-action">
+        <el-button
+          class="action-btn"
+          type="success"
+          @click="toggleVideo"
+        >{{isVideoOn ? '关闭摄像头' : '打开摄像头'}}</el-button>
+
+        <el-button
+          class="action-btn"
+          type="success"
+          @click="toggleAudio"
+        >{{isAudioOn ? '关闭麦克风' : '打开麦克风'}}</el-button>
+
+        <el-button class="action-btn" type="danger" @click="handleHangup">挂断</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import SearchUser from "../search-user";
+import { getUsernameByUserid } from "../../service";
+import config from "../../config";
+export default {
+  name: "VideoCall",
+  components: {
+    SearchUser
+  },
+  computed: {
+    ...mapState({
+      loginUserInfo: state => state.loginUserInfo,
+      callStatus: state => state.callStatus,
+      isInviter: state => state.isInviter,
+      meetingUserIdList: state => state.meetingUserIdList,
+      muteVideoUserIdList: state => state.muteVideoUserIdList,
+      muteAudioUserIdList: state => state.muteAudioUserIdList
+    })
+  },
+  data() {
+    return {
+      isShowVideoCall: false,
+      isVideoOn: true,
+      isAudioOn: true,
+      userId2Name: {}
+    };
+  },
+  mounted() {
+    if (this.callStatus === "connected" && !this.isInviter) {
+      this.startMeeting();
+      this.updateUserId2Name(this.meetingUserIdList);
+    }
+  },
+  destroyed() {
+    this.$store.commit("updateMuteVideoUserIdList", []);
+    this.$store.commit("updateMuteAudioUserIdList", []);
+    if (this.callStatus === "connected") {
+      this.$trtcCalling.hangup();
+      this.$store.commit("updateCallStatus", "idle");
+    }
+  },
+  watch: {
+    callStatus: function(newStatus, oldStatus) {
+      // 作为被邀请者, 建立通话连接
+      if (newStatus !== oldStatus && newStatus === "connected") {
+        this.startMeeting();
+        this.updateUserId2Name(this.meetingUserIdList);
+      }
+    },
+    meetingUserIdList: function(newList, oldList) {
+      if (newList !== oldList || newList.length !== oldList) {
+        this.updateUserId2Name(newList);
+      }
+    }
+  },
+  methods: {
+    handleCallUser: function({ param }) {
+      // 可设置超时
+      this.$trtcCalling.call({
+        userID: param,
+        type: this.TrtcCalling.CALL_TYPE.VIDEO_CALL,
+        timeout: config.CallTimeout
+      });
+      this.$store.commit("userJoinMeeting", this.loginUserInfo.userId);
+      this.$store.commit("updateCallStatus", "calling");
+      this.$store.commit("updateIsInviter", true);
+    },
+    handleCancelCallUser: function() {
+      this.$trtcCalling.hangup();
+      this.$store.commit("dissolveMeeting");
+      this.$store.commit("updateCallStatus", "idle");
+    },
+    startMeeting: function() {
+      if (this.meetingUserIdList.length >= 3) {
+        // 多人通话
+        const lastJoinUser = this.meetingUserIdList[
+          this.meetingUserIdList.length - 1
+        ];
+        this.$trtcCalling.startRemoteView({
+          userID: lastJoinUser,
+          videoViewDomID: `video-${lastJoinUser}`
+        });
+        return;
+      }
+      this.isShowVideoCall = true;
+      this.$trtcCalling.startLocalView({
+        userID: this.loginUserInfo.userId,
+        videoViewDomID: `video-${this.loginUserInfo.userId}`
+      });
+      const otherParticipants = this.meetingUserIdList.filter(
+        userId => userId !== this.loginUserInfo.userId
+      );
+      otherParticipants.forEach(userId => {
+        this.$trtcCalling.startRemoteView({
+          userID: userId,
+          videoViewDomID: `video-${userId}`
+        });
+      });
+    },
+    handleHangup: function() {
+      this.$trtcCalling.hangup();
+      this.isShowVideoCall = false;
+      this.$store.commit("updateCallStatus", "idle");
+      this.$router.push("/");
+    },
+    toggleVideo: function() {
+      this.isVideoOn = !this.isVideoOn;
+      if (this.isVideoOn) {
+        this.$trtcCalling.openCamera();
+        const muteUserList = this.muteVideoUserIdList.filter(
+          userId => userId !== this.loginUserInfo.userId
+        );
+        this.$store.commit("updateMuteVideoUserIdList", muteUserList);
+      } else {
+        this.$trtcCalling.closeCamera();
+        const muteUserList = this.muteVideoUserIdList.concat(
+          this.loginUserInfo.userId
+        );
+        this.$store.commit("updateMuteVideoUserIdList", muteUserList);
+      }
+    },
+    toggleAudio: function() {
+      this.isAudioOn = !this.isAudioOn;
+      this.$trtcCalling.setMicMute(!this.isAudioOn);
+      if (this.isAudioOn) {
+        const muteUserList = this.muteAudioUserIdList.filter(
+          userId => userId !== this.loginUserInfo.userId
+        );
+        this.$store.commit("updateMuteAudioUserIdList", muteUserList);
+      } else {
+        const muteUserList = this.muteAudioUserIdList.concat(
+          this.loginUserInfo.userId
+        );
+        this.$store.commit("updateMuteAudioUserIdList", muteUserList);
+      }
+    },
+    isUserMute: function(muteUserList, userId) {
+      return muteUserList.indexOf(userId) !== -1;
+    },
+    updateUserId2Name: async function(userIdList) {
+      let userId2Name = {};
+      let loginUserId = this.loginUserInfo.userId;
+      for (let i = 0; i < userIdList.length; i++) {
+        const userId = userIdList[i];
+        if (!this.userId2Name[userId]) {
+          const userName = await getUsernameByUserid(userId);
+          userId2Name[userId] = userName;
+          if (loginUserId === userId) {
+            userId2Name[userId] += "(me)";
+          }
+        }
+      }
+      this.userId2Name = {
+        ...this.userId2Name,
+        ...userId2Name
+      };
+    },
+    goto: function(path) {
+      this.$router.push(path);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.video-call-section {
+  padding-top: 50px;
+  width: 800px;
+  margin: 0 auto;
+}
+.video-call-section-header {
+  font-size: 24px;
+}
+.video-call-section-title {
+  margin-top: 30px;
+  font-size: 20px;
+}
+.video-conference {
+  display: none;
+  margin-top: 20px;
+}
+.video-conference.is-show {
+  display: block;
+}
+
+.video-conference-list {
+  display: flex;
+  flex-direction: row;
+  margin-top: 10px;
+}
+
+.user-video-container {
+  position: relative;
+  text-align: left;
+  width: 360px;
+  height: 240px;
+  margin: 10px;
+}
+
+.user-video-status {
+  position: absolute;
+  right: 50px;
+  bottom: 20px;
+  width: 24px;
+  height: 27px;
+  z-index: 10;
+  background-image: url("../../assets/camera-on.png");
+  background-size: cover;
+}
+.user-video-status.is-mute {
+  background-image: url("../../assets/camera-off.png");
+}
+
+.user-audio-status {
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+  width: 22px;
+  height: 27px;
+  z-index: 10;
+  background-image: url("../../assets/mic-on.png");
+  background-size: cover;
+}
+
+.user-audio-status.is-mute {
+  background-image: url("../../assets/mic-off.png");
+}
+
+.video-conference-action {
+  margin-top: 10px;
+}
+
+.video-item-username {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  z-index: 10;
+  color: #ffffff;
+}
+@media screen and (max-width: 767px) {
+  .video-call-section {
+    width: 100%;
+  }
+  .video-conference-list {
+    margin: 0;
+    padding: 10px;
+  }
+  .user-video-container {
+    margin: 5px;
+  }
+}
+</style>
