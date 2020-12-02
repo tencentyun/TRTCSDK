171c575ad57bb739eab2dd476ca4f3db911bc033
finnguan <finnguan@tencent.com>
2020-12-02 21:49:47 +0800

    【Android】升级bugly，增加安装包升级策略

--- Android/TRTCScenesDemo/app/src/main/java/com/tencent/liteav/demo/DemoApplication.java
+++ Android/TRTCScenesDemo/app/src/main/java/com/tencent/liteav/demo/DemoApplication.java
@@ -3,7 +3,13 @@ package com.tencent.liteav.demo;
 import android.os.Build;
 import android.os.StrictMode;
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
 
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.download.DownloadListener;
+import com.tencent.bugly.beta.download.DownloadTask;
+import com.tencent.bugly.beta.upgrade.UpgradeStateListener;
 import com.tencent.bugly.crashreport.CrashReport;
 import com.tencent.rtmp.TXLiveBase;
 
@@ -16,6 +22,14 @@ import java.lang.reflect.Method;
 
 
 public class DemoApplication extends MultiDexApplication {
+    private static String TAG = "DemoApplication";
+
+    //配置bugly组件的appId
+    private static final String BUGLY_APPID = "";
+    // 配置bugly组件的APP渠道号
+    private static final String BUGLY_APP_CHANNEL = "";
+    //配置bugly组件的调试模式（true或者false）
+    private static final boolean BUGLY_ENABLE_DEBUG = true;
 
     //    private RefWatcher mRefWatcher;
     private static DemoApplication instance;
@@ -32,10 +46,7 @@ public class DemoApplication extends MultiDexApplication {
         instance = this;
 
         TXLiveBase.setConsoleEnabled(true);
-        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());
-        strategy.setAppVersion(TXLiveBase.getSDKVersionStr());
-        CrashReport.initCrashReport(getApplicationContext(), strategy);
-
+        initBugly();
         TXLiveBase.getInstance().setLicence(instance, licenceUrl, licenseKey);
 
         // 短视频licence设置
@@ -75,6 +86,58 @@ public class DemoApplication extends MultiDexApplication {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    //配置bugly组件的APP ID，bugly组件为腾讯提供的用于crash上报,分析和升级的开放组件，如果您不需要该组件，可以自行移除
+    private void initBugly() {
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());
+        strategy.setAppVersion(TXLiveBase.getSDKVersionStr());
+        strategy.setAppChannel(BUGLY_APP_CHANNEL);
+        //监听安装包下载状态
+        Beta.downloadListener = new DownloadListener() {
+            @Override
+            public void onReceive(DownloadTask downloadTask) {
+            }
+
+            @Override
+            public void onCompleted(DownloadTask downloadTask) {
+                Log.d(TAG,"downloadListener download apk file success");
+            }
+
+            @Override
+            public void onFailed(DownloadTask downloadTask, int i, String s) {
+                Log.d(TAG,"downloadListener download apk file fail");
+            }
+        };
+
+        //监听APP升级状态
+        Beta.upgradeStateListener = new UpgradeStateListener(){
+            @Override
+            public void onUpgradeFailed(boolean b) {
+                Log.d(TAG,"upgradeStateListener upgrade failed");
+            }
+
+            @Override
+            public void onUpgradeSuccess(boolean b) {
+                Log.d(TAG,"upgradeStateListener upgrade success");
+            }
+
+            @Override
+            public void onUpgradeNoVersion(boolean b) {
+                Log.d(TAG,"upgradeStateListener upgrade has no new version");
+            }
+
+            @Override
+            public void onUpgrading(boolean b) {
+                Log.d(TAG,"upgradeStateListener upgrading");
+            }
+
+            @Override
+            public void onDownloadCompleted(boolean b) {
+                Log.d(TAG,"upgradeStateListener download apk file success");
+            }
+        };
+        Bugly.init(getApplicationContext(), BUGLY_APPID, BUGLY_ENABLE_DEBUG, strategy);
     }
 
 }
