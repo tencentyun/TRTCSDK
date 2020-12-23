58860aaeb558b6f0211bbed9386d883c62750670
finnguan <finnguan@tencent.com>
2020-12-23 19:29:40 +0800

    SDK 更新至 8.2.9808

--- Android/TRTCScenesDemo/app/src/main/java/com/tencent/liteav/demo/TRTCMainActivity.java
+++ Android/TRTCScenesDemo/app/src/main/java/com/tencent/liteav/demo/TRTCMainActivity.java
@@ -68,7 +68,7 @@ public class TRTCMainActivity extends Activity {
         }
         setContentView(R.layout.activity_trtc_main);
         mTvVersion = (TextView) findViewById(R.id.main_tv_version);
-        mTvVersion.setText("腾讯云 TRTC v" + TXLiveBase.getSDKVersionStr()+"(8.1.624)");
+        mTvVersion.setText("腾讯云 TRTC v" + TXLiveBase.getSDKVersionStr()+"(8.2.638)");
         mMainTitle = (TextView) findViewById(R.id.main_title);
         mMainTitle.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
