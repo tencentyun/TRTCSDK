5543017b3b21458d9fea1380cb131cc9f4b2c0c8
tyrosning <tyrosning@tencent.com>
2021-11-11 19:27:16 +0800

    feat: TUICalling V1.0.0

--- WXMini/TRTCScenesDemo/trtc-calling-miniapp/pages/videoCall/videoCall.js
+++ WXMini/TRTCScenesDemo/trtc-calling-miniapp/pages/videoCall/videoCall.js
@@ -10,6 +10,7 @@ Page({
       userID: wx.$globalData.userID,
       userSig: wx.$globalData.userSig,
       type: 2,
+      tim: null,
     },
   },
 
@@ -39,95 +40,19 @@ Page({
       })
       return
     }
-    this.TRTCCalling.call({ userID: this.data.invitee.userID, type: 2 })
-  },
-
-  invitedEvent() {},
-
-  hangupEvent() {},
-
-  rejectEvent() {
-    wx.showToast({
-      title: '对方已拒绝',
-    })
-  },
-
-  userLeaveEvent() {},
-
-  onRespEvent() {
-    wx.showToast({
-      title: '对方不在线',
-    })
-    this.TRTCCalling.hangup()
-  },
-
-  callingTimeoutEvent() {
-    wx.showToast({
-      title: '无应答超时',
-    })
-  },
-
-  lineBusyEvent() {
-    wx.showToast({
-      title: '对方忙线中',
-    })
-    this.TRTCCalling.hangup()
-  },
-
-  callingCancelEvent() {
-    wx.showToast({
-      title: '通话已取消',
-    })
-  },
-
-  userEnterEvent() {},
-
-  callEndEvent() {
-    wx.showToast({
-      title: '通话结束',
-    })
-    this.TRTCCalling.hangup()
-  },
-
-  bindTRTCCallingRoomEvent: function() {
-    const TRTCCallingEvent = this.TRTCCalling.EVENT
-    this.TRTCCalling.on(TRTCCallingEvent.INVITED, this.invitedEvent)
-    // 处理挂断的事件回调
-    this.TRTCCalling.on(TRTCCallingEvent.HANG_UP, this.hangupEvent)
-    this.TRTCCalling.on(TRTCCallingEvent.REJECT, this.rejectEvent)
-    this.TRTCCalling.on(TRTCCallingEvent.USER_LEAVE, this.userLeaveEvent)
-    this.TRTCCalling.on(TRTCCallingEvent.NO_RESP, this.onRespEvent)
-    this.TRTCCalling.on(TRTCCallingEvent.CALLING_TIMEOUT, this.callingTimeoutEvent)
-    this.TRTCCalling.on(TRTCCallingEvent.LINE_BUSY, this.lineBusyEvent)
-    this.TRTCCalling.on(TRTCCallingEvent.CALLING_CANCEL, this.callingCancelEvent)
-    this.TRTCCalling.on(TRTCCallingEvent.USER_ENTER, this.userEnterEvent)
-    this.TRTCCalling.on(TRTCCallingEvent.CALL_END, this.callEndEvent)
-  },
-  unbindTRTCCallingRoomEvent() {
-    const TRTCCallingEvent = this.TRTCCalling.EVENT
-    this.TRTCCalling.off(TRTCCallingEvent.INVITED, this.invitedEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.HANG_UP, this.hangupEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.REJECT, this.rejectEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.USER_LEAVE, this.userLeaveEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.NO_RESP, this.onRespEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.CALLING_TIMEOUT, this.callingTimeoutEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.LINE_BUSY, this.lineBusyEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.CALLING_CANCEL, this.callingCancelEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.USER_ENTER, this.userEnterEvent)
-    this.TRTCCalling.off(TRTCCallingEvent.CALL_END, this.callEndEvent)
+    this.TUICalling.call({ userID: this.data.invitee.userID, type: 2 })
   },
 
   onBack: function() {
     wx.navigateBack({
       delta: 1,
     })
-    this.TRTCCalling.logout()
+    this.TUICalling.destroyed()
   },
 
 
   onLoad: function() {
     const Signature = genTestUserSig(wx.$globalData.userID)
-    console.log('audio----config', wx.$globalData)
     const config = {
       sdkAppID: wx.$globalData.sdkAppID,
       userID: wx.$globalData.userID,
@@ -136,19 +61,15 @@ Page({
     this.setData({
       config: { ...this.data.config, ...config },
     }, () => {
-      this.TRTCCalling = this.selectComponent('#TRTCCalling-component')
-      this.bindTRTCCallingRoomEvent()
-      this.TRTCCalling.login()
+      this.TUICalling = this.selectComponent('#TUICalling-component')
+      this.TUICalling.init()
     })
   },
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function() {
-    // 取消监听事件
-    this.unbindTRTCCallingRoomEvent()
-    // 退出登录
-    this.TRTCCalling.logout()
+    this.TUICalling.destroyed()
   },
 
   onShow: function() {
