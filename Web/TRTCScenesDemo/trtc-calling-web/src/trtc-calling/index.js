7b1578a21cad7d16fcb57664e1ee7a34b0fa1b1d
longyuqi <longyuqi@tencent.com>
2022-12-14 14:39:24 +0800

    chore: deprecated

--- Web/TRTCScenesDemo/trtc-calling-web/src/trtc-calling/index.js
+++
@@ -1,8 +0,0 @@
-import TRTCCalling from 'trtc-calling-js';
-import config from '../config';
-
-export function createTrtcCalling() {
-  return new TRTCCalling({
-    SDKAppID: config.SDKAppID
-  });
-}
--- Web/TRTCScenesDemo/trtc-calling-web/vue.config.js
+++
@@ -1,4 +0,0 @@
-// vue.config.js
-module.exports = {
-  publicPath: './'
-}
